f0301a864c3eae395829a153b53ef7f9f396c158
Atamagai <99071577@mydavinci.nl>
2024-11-11 10:19:33 +0100

    Mqtt uitgebreid

--- Django/Dashboard_ASVZ/Dashboard_ASVZ/Dashboard_ASVZ/settings.py
+++ Django/Dashboard_ASVZ/Dashboard_ASVZ/Dashboard_ASVZ/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'authentication',
-    'application',
+    'application.apps.ApplicationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Django/Dashboard_ASVZ/Dashboard_ASVZ/application/apps.py
+++ Django/Dashboard_ASVZ/Dashboard_ASVZ/application/apps.py
@@ -1,6 +1,20 @@
+# application/apps.py
 from django.apps import AppConfig
-
+import threading
 
 class ApplicationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'application'
+
+    def ready(self):
+        # Start the MQTT listener in a separate thread
+        mqtt_thread = threading.Thread(target=self.start_mqtt_listener)
+        mqtt_thread.daemon = True  # Ensures the thread exits with the server
+        mqtt_thread.start()
+
+    def start_mqtt_listener(self):
+        # Delay importing `MqttService` until Django is fully loaded
+        from .mqtt_service import MqttService
+
+        # Initialize and start the MQTT listener
+        mqtt_service = MqttService()
+        mqtt_service.listen()
